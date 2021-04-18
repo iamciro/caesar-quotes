8a407819435471f634d295bb3f6d80ded6b7e4d8
Ciro Goyeneche <soycirog@gmail.com>
2021-04-18 01:44:24 -0300

    get_quote function created

--- caesar/utils.py
+++ caesar/utils.py
@@ -1,3 +1,15 @@
 """
     This file contains utils functions
 """
+import requests
+import json
+
+API_END_POINT = "https://caesar-api.herokuapp.com/quote"
+
+def get_quote():
+    try:
+        response = requests.get(API_END_POINT)
+        data = json.loads(response.text)
+        return data['quote']
+    except KeyError:
+        return None
