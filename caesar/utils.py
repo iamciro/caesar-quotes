0b249984248e1315c328a2ecdac36782b8bd6d29
Ciro Goyeneche <soycirog@gmail.com>
2021-04-18 01:52:42 -0300

    ConnectionError handling

--- caesar/utils.py
+++ caesar/utils.py
@@ -7,9 +7,14 @@ import json
 API_END_POINT = "https://caesar-api.herokuapp.com/quote"
 
 def get_quote():
+    """
+        Get quote from API
+    """
     try:
         response = requests.get(API_END_POINT)
         data = json.loads(response.text)
         return data['quote']
     except KeyError:
+        return None
+    except requests.exceptions.ConnectionError:
         return None
