1624039ad633fedf4259db1d6d6c0fef058b7696
Ciro Goyeneche <soycirog@gmail.com>
2021-04-18 01:44:47 -0300

    get_cuote function imported and used

--- caesar/routes.py
+++ caesar/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from caesar import app
+from .utils import get_quote
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    quote = get_quote()
+    return render_template('index.html', quote=quote)
